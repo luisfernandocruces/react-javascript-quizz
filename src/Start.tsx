bc09fa122ae31b452d6c565a82d7af9db08dc23e
lucho <luisfcv97@gmail.com>
2023-05-04 01:41:35 -0500

    feat: implement questions store

---
+++ src/Start.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import { useQuestionsStore } from './store/questions'
+
+export const Start = () => {
+  const fetchQuestions = useQuestionsStore(state => state.fetchQuestions)
+  const handleClick = () => {
+    fetchQuestions(5)
+  }
+  return (
+    <Button onClick={handleClick} variant='contained'>Start</Button>
+  )
+}
---
+++ src/store/questions.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+import { Question } from '../types'
+
+interface State {
+  questions: Question[]
+  currentQuestion: number
+  fetchQuestions: (limit: number) => Promise<void>
+}
+
+export const useQuestionsStore = create<State>((set, get) => ({
+  // initial state
+  questions: [],
+  currentQuestion: 0,
+  fetchQuestions: async (limit: number) => {
+    const res = await fetch('http://localhost:5173/data.json')
+    const json = await res.json()
+    const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
+    set({ questions })
+  }
+}))
